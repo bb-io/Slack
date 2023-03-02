de9d56bd0d7d0cc55a70c5204263d30b9dbca567
msonnemans <mathijssonnemans@hotmail.com>
2023-03-02 12:59:46 +0100

    Add project files.

---
+++ Apps.Slack/Actions.cs
@@ -0,0 +1,21 @@
+﻿using Apps.Slack.Models;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using RestSharp;
+
+namespace Apps.Slack
+{
+    [ActionList]
+    public class Actions
+    {
+        [Action]
+        public void PostMessage(AuthenticationCredentialsProvider authenticationCredentialsProvider, [ActionParameter] MessageParameters input)
+        {
+            var client = new RestClient("https://slack.com/api");
+            var request = new RestRequest("/chat.postMessage", Method.Post);
+            request.AddHeader("Authorization", $"Bearer {authenticationCredentialsProvider.Value}");
+            request.AddJsonBody(new MessageRequest { Channel = input.ChannelId, Text = input.Text });
+            client.Post(request);
+        }
+    }
+}
---
+++ Apps.Slack/Connections/ConnectionProviderFactory.cs
@@ -0,0 +1,13 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication;
+
+namespace Apps.Slack.Connections
+{
+    public class ConnectionProviderFactory : IConnectionProviderFactory
+    {
+        public IEnumerable<IConnectionProvider> Create()
+        {
+            yield return new ConnectionProvider();
+        }
+    }
+}
