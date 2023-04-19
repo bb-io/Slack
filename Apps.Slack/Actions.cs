1f844d5b70fda9b40428e22f2f5b7d1b87283461
Lyubka Kashkavelina <LKashkavelina@nemetschek.bg>
2023-04-19 14:37:56 +0300

    Adding a new connection definition for Slack application in order to work with SDK 2.0

--- Apps.Slack/Actions.cs
+++ Apps.Slack/Actions.cs
@@ -1,5 +1,6 @@
 ﻿using Apps.Slack.Models;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using RestSharp;
 
@@ -10,25 +11,34 @@ namespace Apps.Slack
     {
       
         [Action( "Post message to slack", Description = "Post message to slack")]
-        public void PostMessage(AuthenticationCredentialsProvider authenticationCredentialsProvider, [ActionParameter] MessageParameters input)
+        public void PostMessage(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders, [ActionParameter] MessageParameters input)
         {
             var client = new SlackClient();
             var request = new RestRequest("/chat.postMessage", Method.Post);
+            var authenticationCredentialsProvider = GetAuthenticationCredentialsProvider(authenticationCredentialsProviders);
+
             request.AddHeader("Authorization", $"Bearer {authenticationCredentialsProvider.Value}");
             request.AddJsonBody(new MessageRequest { Channel = input.ChannelId, Text = input.Text });
             client.Post(request);
         }
         
         [Action("Upload file", Description = "Upload file to channel")]
-        public void UploadFile(AuthenticationCredentialsProvider authenticationCredentialsProvider, [ActionParameter] UploadFileDto input)
+        public void UploadFile(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders, [ActionParameter] UploadFileDto input)
         {
             var client = new SlackClient();
             var request = new RestRequest("/files.upload", Method.Post);
+            var authenticationCredentialsProvider = GetAuthenticationCredentialsProvider(authenticationCredentialsProviders);
+
             request.AddHeader("Authorization", $"Bearer {authenticationCredentialsProvider.Value}");
             request.AddParameter("channels", input.ChannelId);
             request.AddParameter("filename", input.FileName);
             request.AddFile("file", input.File, input.FileName, input.FileType);
             client.Post(request);
+        }
+
+        private AuthenticationCredentialsProvider GetAuthenticationCredentialsProvider(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
+        {
+            return authenticationCredentialsProviders.First(p => p.KeyName == "token");
         }
     }
 }
