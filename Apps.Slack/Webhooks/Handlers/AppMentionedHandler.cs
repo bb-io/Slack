5f4e3ed63da9eedfe1c569e9a71d3f30c4323db6
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-27 19:10:21 +0300

    workaround for webhooks

--- Apps.Slack/Webhooks/Handlers/AppMentionedHandler.cs
+++ Apps.Slack/Webhooks/Handlers/AppMentionedHandler.cs
@@ -1,11 +1,24 @@
 ﻿using Apps.Slack.Webhooks.Handlers.Base;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
 
 namespace Apps.Slack.Webhooks.Handlers;
 
-public class AppMentionedHandler : BaseWebhookHandler
-{
+public class AppMentionedHandler : BaseInvocable, IWebhookEventHandler
+{ 
     const string SubscriptionEvent = "app_mention";
 
-    public AppMentionedHandler(InvocationContext invocationContext) : base(invocationContext, SubscriptionEvent) { }
+    public AppMentionedHandler(InvocationContext invocationContext) : base(invocationContext) { }
+
+    public Task SubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider, Dictionary<string, string> values)
+    {
+        return BaseWebhookHandler.SubscribeAsync(authenticationCredentialsProvider, values, $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/slack", SubscriptionEvent);
+    }
+
+    public Task UnsubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider, Dictionary<string, string> values)
+    {
+        return BaseWebhookHandler.UnsubscribeAsync(authenticationCredentialsProvider, values, $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/slack", SubscriptionEvent);
+    }
 }
