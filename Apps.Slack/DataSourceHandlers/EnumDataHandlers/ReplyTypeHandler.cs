2627a28b3d20536bc8d8ecb0df4ba95bf4c78c24
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-02-20 16:56:14 +0100

    Version bump

--- Apps.Slack/DataSourceHandlers/EnumDataHandlers/ReplyTypeHandler.cs
+++ Apps.Slack/DataSourceHandlers/EnumDataHandlers/ReplyTypeHandler.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Apps.Slack.DataSourceHandlers.EnumDataHandlers
 {
-    internal class ReplyTypeHandlerIDataSourceHandler
+    public class ReplyTypeHandlerIDataSourceHandler
     {
         public Dictionary<string, string> GetData(DataSourceContext context)
         {
