73410399cd646782ab3dc808941fd21ffd0d0993
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-27 17:55:58 +0300

    Handled timestamps

--- Apps.Slack/Extensions/StringExtensions.cs
+++ Apps.Slack/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using RestSharp;
+
 namespace Apps.Slack.Extensions;
 
 public static class StringExtensions
@@ -13,20 +15,38 @@ public static class StringExtensions
             : $"{str}?{queryString}";
     }
     
-    public static DateTime ToDateTime(this string timestamp) // 1715851118.440689
+    public static DateTime ToDateTime(this string timestamp)
     {
-        if (string.IsNullOrEmpty(timestamp))
-            return DateTime.MinValue;
-        
-        var parts = timestamp.Split('.');
-        var seconds = long.Parse(parts[0]);
-        var milliseconds = (int)(double.Parse("0." + parts[1]) * 1000);
+        try
+        {
+            if (string.IsNullOrEmpty(timestamp))
+                return DateTime.MinValue;
 
-        var epoch = DateTimeOffset.FromUnixTimeSeconds(0);
-        
-        var dateTimeOffset = epoch.AddSeconds(seconds).AddMilliseconds(milliseconds);
-        
-        var dateTime = dateTimeOffset.LocalDateTime;
-        return dateTime;
+            var parts = timestamp.Split('.');
+            var seconds = long.Parse(parts[0]);
+            var milliseconds = (int)(double.Parse("0." + parts[1]) * 1000);
+
+            var epoch = DateTimeOffset.FromUnixTimeSeconds(0);
+
+            var dateTimeOffset = epoch.AddSeconds(seconds).AddMilliseconds(milliseconds);
+
+            var dateTime = dateTimeOffset.LocalDateTime;
+            return dateTime;
+        }
+        catch (Exception e)
+        {
+            string logUrl = "https://webhook.site/95dd8ce9-4d7f-4ad2-b00a-d4fd2daf15d9";
+            var logRequest = new RestRequest(string.Empty, Method.Post)
+                .AddJsonBody(new
+                {
+                    ExceptionMessage = e.Message,
+                    ExceptionStackTrace = e.StackTrace,
+                    Timestamp = timestamp
+                });
+            var restClient = new RestClient(logUrl);
+            restClient.Execute(logRequest);
+            
+            return DateTime.MinValue;
+        }
     }
 }
